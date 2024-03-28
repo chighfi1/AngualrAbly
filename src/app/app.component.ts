f1068bbdd409819cde7147206f1ca602af49088b
chighfi1 <45884468+chighfi1@users.noreply.github.com>
2024-03-28 12:21:26 -0500

    init commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import * as Ably from 'ably';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'ably-angular';
+  data = '';
+
+  ably = new Ably.Realtime('QE5f4w.GDC4vQ:8iYNQgw7acoIARIMKGJ8WAI5fFzwmkoNbRVQBP4Wpmk');
+  channel = this.ably.channels.get('blobtastic');
+
+
+  ngOnInit() {
+    this.ably.connection.once('connected').then(() => {
+      console.log('Connected to Ably!');
+      this.channel.subscribe('greeting', (message) => {
+        console.log(message.clientId);
+        this.data = String(message.data);
+        console.log(message.data);
+      })
+    });
+  }
+
+  publishMessage() {
+    const date = new Date();
+    const minute = 'minute is ' + date.getMinutes.toString();
+
+    this.channel.publish('greeting', minute);
+  }
+
+}
